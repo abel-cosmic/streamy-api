0a5f43b15758a110dbd8129f657f7a1c63166fee
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-03-31 21:13:07 +0300

    fix: fixed the database connection file

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/abel-cosmic/streamy-api/config"
 	"github.com/abel-cosmic/streamy-api/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,21 +12,30 @@ import (
 
 // Connect to the database
 func Connect() *gorm.DB {
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbHost := os.Getenv("DB_HOST")
+	dbName := config.Config("DB_NAME")
+	dbUser := config.Config("DB_USER")
+	dbPass := config.Config("DB_PASS")
+	dbHost := config.Config("DB_HOST")
+	dbPort := config.Config("DB_PORT")
+	fmt.Println("DB_PORT:", dbPort)
+	fmt.Println("DB_USER:", dbUser)
+	fmt.Println("DB_HOST:", dbHost)
+	fmt.Println("DB_NAME:", dbName)
+	fmt.Println("DB_PASS:", dbPass)
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 
-	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, "3306", dbName)
-
+	fmt.Println("Attempting to connect to the database...")
 	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{})
-
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Database connection successful!")
+
 	if os.Getenv("DB_MIGRATE") == "true" {
+		fmt.Println("Migrating database tables...")
 		Migrate(db)
+		fmt.Println("Database migration completed successfully!")
 	}
 
 	return db
@@ -33,5 +43,5 @@ func Connect() *gorm.DB {
 
 // Migrate tables
 func Migrate(db *gorm.DB) {
-	db.Migrator().CreateTable(&model.Album{})
+	db.AutoMigrate(&model.Album{})
 }
